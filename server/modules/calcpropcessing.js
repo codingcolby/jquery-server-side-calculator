175f9920386c9f51d02175be9c5bf74e33ab4717
allthethings <learn2code@respectdesfonds.com>
2020-03-28 19:02:13 -0500

    built calculating function in calcprocessing file and successfully tested in servertest file

--- server/modules/calcpropcessing.js
+++ server/modules/calcpropcessing.js
@@ -1,2 +1,40 @@
+console.log('Hello calcprocessing.js');
+
 // TO DO
 // create a callable function to process the given inputs with the selected operator
+const answer = [];
+
+function calcprocessing (input1, operator, input2) {
+    if (operator === "+") {
+        let result = (Number(input1) + Number(input2));
+        answer.push(result);
+    }
+    else if (operator == "-") {
+        let result = (input1 - input2);
+        answer.push(result);
+    }
+    else if (operator == "*") {
+        let result = (input1 * input2);
+        answer.push(result);
+    }
+    else if (operator == "/") {
+        let result = (input1 / input2);
+        answer.push(result);
+    }
+    else {
+        console.log('A result could not be calculated');
+    }
+}
+
+// TESTS
+// calcprocessing('6', '+', '2');
+// console.log(answer); // should be 8
+
+// calcprocessing('6', '-', '2');
+// console.log(answer); // should be 4
+
+// calcprocessing('6', '*', '2');
+// console.log(answer); // should be 12
+
+// calcprocessing('6', '/', '2');
+// console.log(answer); // should be 3
